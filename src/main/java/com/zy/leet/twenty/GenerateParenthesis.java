fcc9beefa052668c45f13438d3bd54da759921e3
zy-yz <1090712762@qq.com>
2020-01-07 20:01:31 +0800

    leetCode

--- src/main/java/com/zy/leet/twenty/GenerateParenthesis.java
+++ src/main/java/com/zy/leet/twenty/GenerateParenthesis.java
@@ -16,6 +16,7 @@ import java.util.List;
  */
 public class GenerateParenthesis {
 
+    //深度优先遍历
     //做减法
     public static List<String> generateParenthesis(int n) {
 
@@ -57,5 +58,46 @@ public class GenerateParenthesis {
     public static void main(String[] args) {
         generateParenthesis(3);
         System.out.println(generateParenthesis(3));
+    }
+
+    //广度优先bianli
+    //做加法
+    public static List<String> generateParenthesis1(int n){
+        List<String> res = new ArrayList<String>();
+        // 特判
+        if (n == 0) {
+            return res;
+        }
+
+        dfs("", 0, 0, n, res);
+        return res;
+    }
+    /**
+     * @param curStr 当前递归得到的结果
+     * @param left   左括号已经用了几个
+     * @param right  右括号已经用了几个
+     * @param n      左括号、右括号一共得用几个
+     * @param res    结果集
+     */
+    private static void dfs(String curStr, int left, int right, int n, List<String> res) {
+
+        if (left == n && right == n) {
+            res.add(curStr);
+            return;
+        }
+
+        // 剪枝
+        if (left < right) {
+            return;
+        }
+
+        if (left < n) {
+            dfs(curStr + "(", left + 1, right, n, res);
+        }
+        if (right < n) {
+            dfs(curStr + ")", left, right + 1, n, res);
+        }
+
+
     }
 }
