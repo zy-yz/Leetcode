0289e72f327b7cac33e33b9e7b4bfcc22e3debca
zy-yz <1090712762@qq.com>
2020-03-12 01:04:50 +0800

    阅读Arraylist和Linkedlist源码

--- src/main/java/com/zy/leet/collection/TestLinkedList.java
+++ src/main/java/com/zy/leet/collection/TestLinkedList.java
@@ -1,4 +1,14 @@
 package com.zy.leet.collection;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class TestLinkedList {
+    public static void main(String[] args) {
+
+        List<String> linkedList = new LinkedList<>();
+        linkedList.add("3");
+        System.out.println(linkedList.get(0));
+
+    }
 }
