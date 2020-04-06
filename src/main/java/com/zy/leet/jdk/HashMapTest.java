d752001d08151f7cb74c57c368531981e53740ea
zy-yz <1090712762@qq.com>
2020-04-06 22:01:18 +0800

    HashMapTest

--- src/main/java/com/zy/leet/jdk/HashMapTest.java
+++ src/main/java/com/zy/leet/jdk/HashMapTest.java
@@ -14,6 +14,8 @@ public class HashMapTest {
     public static void main(String[] args) {
 
         Map HashMapTest = new HashMap();
+        Map HashMapTest3 = new HashMap(10);
+        Map HashMapTest4 = new HashMap(10, (float) 0.75);
         HashMapTest.put("1",1);
         HashMapTest.put("2",2);
 
@@ -22,6 +24,12 @@ public class HashMapTest {
         Map HashMapTest1 = new HashMap(HashMapTest);
         System.out.println(HashMapTest1.get("1"));
         System.out.println(HashMapTest1.get("2"));
+
+        int f=1,i=2;
+        while (i++<5){
+            f*=i;
+            System.out.println(f);
+        }
     }
 
 }
